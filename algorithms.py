8acb6462aa8c3a9d8ba785a6ddb79e658dc30a96
Cristian C. Spagnuolo <cristian.c.spagnuolo@gmail.com>
2024-02-26 10:42:47 +0100

    compute metrics for the TMDP

--- algorithms.py
+++ algorithms.py
@@ -125,7 +125,7 @@ def SARSA(env:DiscreteEnv, s, a, Q, M=5000):
         - alpha (float): initial learning rate
         - status_step (int): intermediate results flag. Used for the evaluation of the state action value function updates while learning
 """
-def Q_learning_2(env:DiscreteEnv, Q, episodes=5000, alpha=0.5, status_step=5000):
+def Q_learning(env:DiscreteEnv, Q, episodes=5000, alpha=0.5, status_step=5000):
 
     # Initialize the step counter
     nS, nA = Q.shape
@@ -189,159 +189,69 @@ def Q_learning_2(env:DiscreteEnv, Q, episodes=5000, alpha=0.5, status_step=5000)
     return {"Qs": Qs, "visits": visits}
 
 
-def compute_metrics(env, Qs, Q_star):
+"""
+    Compute some metrics for the evaluation of the performance improvement
+    The metrics computed are:
+        - J: list of expected discounted sum of rewards for the original problem over the state action value functions
+        - J_tau: list of expected discounted sum of rewards for the simplified problem with tau over the state action value functions
+        - delta_J: list of differences between J and J_tau over the state action value functions
+        - delta_Q: list of L_inf norm of the differences between the state action value function and the optimal one 
+        - adv_terms: list of advantage terms for the performance improvement lower bound over the state action value functions
+        - diss_terms: list of dissimilarity penalization terms for the performance improvement lower bound over the state action value functions
+        - l_bounds: list of lower bounds for the performance improvement over the state action value functions
+    Args:
+        - env (DiscreteEnv): environment object
+        - Qs (list): list of state action value functions
+        - Q_star (ndarray): optimal state action value function
+        - tau_prime (float): new teleportation probability, default 0 to evaluate the performance improvement in switching to the original problem
+    return (dict): the computed metrics
+"""
+def compute_metrics(env, Qs, Q_star, tau_prime=0.):
+    Qs.append(Q_star)
     metrics = {}
     J = []
     J_tau = []
-    delta_Q = []
     delta_J = []
+
+    delta_Q = []
+
+    adv_terms = []
+    diss_terms = []
     l_bounds = []
     for Q in Qs:
+        # Compute the expected discounted sum of rewards for the original problem and the simplified one with tau
         J.append(get_expected_avg_reward(env.P_mat, get_policy(Q), env.reward, env.gamma, env.mu))
         J_tau.append(get_expected_avg_reward(env.P_mat_tau, get_policy(Q), env.reward, env.gamma, env.mu))
-        delta_Q.append(np.linalg.norm(Q - Q_star, np.inf))
         delta_J.append(J[-1] - J_tau[-1])
+
+        # Compute the L_inf norm of the difference between the state action value function and the optimal one
+        delta_Q.append(np.linalg.norm(Q - Qs[-1], np.inf))
+        
+        # Compute some model based metrics for the evaluation of the performance improvement
         d = compute_d(env.mu, env.P_mat_tau, get_policy(Q), env.gamma)
         delta = compute_delta(d, get_policy(Q))
         U = get_state_action_nextstate_value_function(env.P_mat_tau, env.reward, env.gamma, Q)
         rel_model_adv_hat = compute_relative_model_advantage_function_hat(env.P_mat, env.xi, U)
         dis_rel_model_adv = compute_discounted_distribution_relative_model_advantage_function_from_delta_tau(rel_model_adv_hat, delta, env.tau, 0.)
-        p
-        #model_adv = get_model_advantage_function(env.P_mat_tau, env.reward, env.tmdp.gamma, Q)
+        adv = dis_rel_model_adv/(1-env.gamma)
+        #Advantage term for the performance improvement lower bound
+        adv_terms.append(adv)
+
+        # Compute the dissimilarity penalization term for the performance improvement lower bound
+        dq = get_sup_difference_q(Q)
+        de = get_expected_difference_transition_models(env.P_mat, env.xi, delta)
+        dinf = get_sup_difference_transition_models(env.P_mat, env.xi)
+        diss_pen = env.gamma**2*(env.tau - tau_prime)**2*dq*de*dinf/(2*(1-env.gamma)**2)
+        diss_terms.append(diss_pen)
+
+        # Compute the lower bound for the performance improvement
+        l_bounds.append(adv-diss_pen)
 
     metrics["J"] = J
     metrics["J_tau"] = J_tau
     metrics["delta_Q"] = delta_Q
     metrics["delta_J"] = delta_J
     metrics["l_bounds"] = l_bounds
+    metrics["adv_terms"] = adv_terms
+    metrics["diss_terms"] = diss_terms
     return metrics
-
-def Q_learning(env:Env, s, a, Q, Q_star, M=5000, alpha=0., status_step=200, debug=False, main_p=True):
-    m = 0
-    J_main_p = []
-    J_curr_p = []
-    delta_q = []
-    delta_J = []
-    l_ = []
-    nS, nA = Q.shape
-    visits = np.zeros(nS)
-    dec_alpha = np.ones(nS)*alpha
-    # Q_learning main looph
-    while m < M:
-        # Learning rate initialization
-        visits[s] = visits[s]+1
-        # epsilon update
-        eps = (1 - m/M)**2
-        # Perform a step in the environment, picking action a
-        s_prime, r, d, p =  env.step(a, debug=debug)
-
-        # Policy improvement step
-        # N.B. allowed action is not present in the Env object, must be managed
-        a_prime = eps_greedy(s_prime, Q, eps, env.allowed_actions[s_prime.item()])
-
-        #print("Step:", m, " state:", s, " action:", a, " next state:",s_prime, " reward:",r, " next action:", a_prime)
-        # Evaluation step
-        Q[s,a] = Q[s,a] + dec_alpha[s]*(r + env.gamma*np.max(Q[s_prime, :]) - Q[s,a])
-
-        #Compute performance metrics for evaluation purposes
-        if(m % status_step == 0):
-            # Compute performance on the original problem
-            J_0 = get_expected_avg_reward(env.P_mat, get_policy(Q), env.reward, env.gamma, env.mu)
-            J_main_p.append(J_0)
-
-            # Compute performance on the current problem
-            J_p = get_expected_avg_reward(env.P_mat_tau, get_policy(Q), env.reward, env.gamma, env.mu)
-            J_curr_p.append(J_p)
-
-            ### Compute the lower bound on performance improvement ###
-            # Compute the discount state distribution
-            d = compute_d(env.mu, env.P_mat_tau, get_policy(Q), env.gamma)
-            # Compute the gamma discounted state distribution
-            delta = compute_delta(d, get_policy(Q))
-            # Compute the state value function
-            V = get_value_function(Q) 
-            # Compute the expected reward when picking action a in state s
-            r_s_a = compute_r_s_a(env.P_mat_tau, env.reward)
-            # Compute the state action next-state value function U_tau(s,a,s') = R(s,a) + \gamma*V_tau(s')
-            U = compute_state_action_nextstate_value_function(nS, nA, r_s_a, env.gamma, V)
-            # Rebuild Q using U as Q_tau(s,a) = \sum{s' \in S}P_tau(s'|s,a)*U_tau(s,a,s')
-            Q_t = rebuild_Q_from_U(env.P_mat_tau, U)
-
-            # Compute the relative model advantage function hat \hat{A}_{tau, mu}(s,a)
-            A_tau_hat = compute_relative_model_advantage_function_hat(env.P_mat, env.xi, U)
-            # Compute the discounted distribution relative model advantage function hat \hat{A}_{tau, mu}
-            A_hat = compute_discounted_distribution_relative_model_advantage_function_hat(A_tau_hat, delta)
-            # The dissimilarity term D = D_e * gamma * D_inf is upperbounded by 4*gamma+(tau - tau_1)
-            # Compute Delta Q_tau as the superior among the difference of the L_1 norm of elements of Q_tau
-            d_q_t = get_sup_difference_q(Q_t)
-            
-            # Compute the performance improvement lower bound when moving to tau=0
-            l_b = compute_performance_improvement_lower_bound(A_hat, env.gamma, d_q_t, env.tau, 0)
-
-            l_bounds.append(l_b)
-
-            # Compute the empirical performance improvement when moving to tau=0
-            delta_J.append(J_0 - J_p )
-
-            delta_q.append(np.linalg.norm(Q - Q_star, np.inf))
-
-        # Setting next iteration
-        m = m+1
-        s = s_prime
-        a = a_prime
-        dec_alpha[s] = max(0, alpha*(1- visits[s]*nS/M))
-    return Q, J_main_p, J_curr_p, delta_q, delta_J, l_bounds
-
-"""
-    Q_learning algorithm implementation
-        - env: environment object
-        - s: current state
-        - a: first action to be taken
-        - Q: current state-action value function
-        - M: number of iterations to be considered
-        - status: intermediate results flag
-        return the state action value function under the pseudo-optimal policy found
-"""
-def batch_Q_learning(env:Env, s, a, Q, Q_star, batch_size, M=5000, status_step=200):
-    assert M % batch_size == 0
-    batch_Q = Q.copy()
-    m = 1
-    # SARSA main loop
-    J = []
-    delta_q = []
-    while m < M:
-        # Learning rate initialization
-        alpha = (1- m/M)
-        # epsilon update
-        eps = (1 - m/M)**2
-        # Perform a step in the environment, picking action a
-        s_prime, r, d, p = env.step(a)
-
-        # Policy improvement step
-        # N.B. allowed action is not present in the Env object, must be managed
-        a_prime = eps_greedy(s_prime, Q, eps, env.allowed_actions[s_prime.item()])
-
-        #print("Step:", m, " state:", s, " action:", a, " next state:",s_prime, " reward:",r, " next action:", a_prime)
-        # Evaluation step
-        batch_Q[s,a] = batch_Q[s,a] + alpha*(r + env.gamma*np.max(batch_Q[s_prime, :]) - batch_Q[s,a])
-        if m % batch_size == 0:
-            Q[s,a] = Q[s,a] + alpha*(r + env.gamma*np.max(batch_Q[s_prime, :]) - Q[s,a])
-        if(m % status_step == 0):
-            J.append(get_expected_avg_reward(env.P_mat, get_policy(Q), env.reward, env.gamma, env.mu))
-            delta_q.append(np.linalg.norm(Q - Q_star, np.inf))
-        # Setting next iteration
-        m = m+1
-        s = s_prime
-        a = a_prime
-    return Q, J, delta_q
-
-"""
-    Compare two different policies in terms of a distance measure
-        - measure: the measure to be used for the comparison
-        - pi: a policy row vector
-        - pi_prime: a second policy row vector
-        return the difference, according to the given measure, of the two policies
-"""
-def compare_policies(measure:DistanceMeasure, pi, pi_prime):
-    return measure.compute_distance(pi, pi_prime)
-
